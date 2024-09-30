f84cdcfffb72e6b455110c11884a95159c9404d3
Aliyah M Smith <aliyahms@umich.edu>
2024-09-30 07:56:17 -0400

    Added an Authentication component and a Sign up form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
-
+import SignUpForm from "./SignUpForm";
+import Authenticate from "./Authenticate";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
 }
 
 export default App;
